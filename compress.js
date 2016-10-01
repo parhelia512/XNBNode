224c7f977829cd2bb62db0aeaad431bf488b944e
Nicholas Lange <nick.t.lange@gmail.com>
2016-10-01 03:12:22 -0700

    Make exporting right correctly.

--- compress.js
+++ compress.js
@@ -5,6 +5,9 @@ exports.decompress = function decompress(compressedBuffer, decompressedBuffer) {
     lz4.decodeBlock(compressedBuffer, decompressedBuffer);
 };
 
-exports.compress = function compress(decompressedBuffer, compressedBuffer) {
-    lz4.encodeBlock(decompressedBuffer, compressedBuffer);
+exports.compress = function compress(decompressedBuffer) {
+    let compressedBuffer = new Buffer(lz4.encodeBound(decompressedBuffer.length));
+    let compressedSize = lz4.encodeBlock(decompressedBuffer, compressedBuffer);
+    compressedBuffer = compressedBuffer.slice(0, compressedSize);
+    return compressedBuffer;
 };
